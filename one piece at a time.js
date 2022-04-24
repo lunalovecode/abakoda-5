1d23cd9eeee6184eebeadc6affca5e6fa6241888
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2022-04-24 11:07:19 +0800

    upload files

---
+++ one piece at a time.js
@@ -0,0 +1,16 @@
+var c = readline();
+if (c === "I") {
+    print("#.\n#.\n#.\n#.");
+} else if (c === "O") {
+    print("##\n##\n..\n..");
+} else if (c === "J") {
+    print(".#\n.#\n##\n..");
+} else if (c === "L") {
+    print("#.\n#.\n##\n..");
+} else if (c === "S") {
+    print("#.\n##\n.#\n..");
+} else if (c === "T") {
+    print("#.\n##\n#.\n..");
+} else if (c === "Z") {
+    print(".#\n##\n#.\n..");
+}
